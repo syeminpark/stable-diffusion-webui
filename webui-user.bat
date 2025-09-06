3d4ab63ef7aebde99c7061a1da1a1d981171e3c7
syeminPark <soccer0874@gmail.com>
2025-09-07 01:37:39 +0900

    save

--- webui-user.bat
+++ webui-user.bat
@@ -3,5 +3,5 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--api --xformers --listen --medvram --no-half-vae --port 7860
+set COMMANDLINE_ARGS=--api --xformers --listen --medvram  --no-half-vae --enable-insecure-extension-access --port 7860
 call webui.bat
